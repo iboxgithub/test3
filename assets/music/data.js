d83db89cc84589d7f6fa52338a2a063d5d3191c8
iboxgithub <iboxnet@mail.com>
2022-02-11 16:03:11 +0000

    rn player ko on web - cant resolve platform

---
+++ assets/music/data.js
@@ -0,0 +1,33 @@
+export const musiclibrary = [
+    {
+      url: 'https://www.bensound.com/bensound-music/bensound-happyrock.mp3',
+      title: 'Happy Rock',
+      artist: 'Benjamin Tissot',
+      album: "Bensound's rock",
+      genre: 'Rock',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://www.bensound.com/bensound-img/happyrock.jpg', // Load artwork from the network
+      duration: 105, // Duration in seconds
+    },
+    {
+      url: 'https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3',
+      title: 'Punky',
+      artist: 'Benjamin Tissot',
+      album: "Bensound's rock",
+      genre: 'Rock',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://www.bensound.com/bensound-img/punky.jpg', 
+      duration: 126, // Duration in seconds
+    },
+    {
+      url: 'https://www.bensound.com/bensound-music/bensound-actionable.mp3',
+      title: 'Actionable',
+      artist: 'Benjamin Tissot',
+      album: "Bensound's rock",
+      genre: 'Rock',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://www.bensound.com/bensound-img/actionable.jpg',
+      duration: 122, // Duration in seconds
+    },
+    
+  ];
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,72 +10,36 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 import { useEffect, useState } from 'react';
-import { 
-  magic, 
-  web3 
-} from '../magic';
-import { abi } from  '../contract/abi.js';
 
-// import TrackPlayer from 'react-native-track-player'
-// import TrackListScreen from './TrackListScreen';
-// import {musiclibrary} from '../assets/music/data.js';
+import TrackPlayer from 'react-native-track-player'
+import TrackListScreen from './TrackListScreen';
+import {musiclibrary} from '../assets/music/data.js'
+
+// import {AppRegistry} from 'react-native';
+// import App from '../App';
+// import {expo.name as appName} from '../app.json';
+
+// AppRegistry.registerComponent('test3', () => App);
+// TrackPlayer.registerPlaybackService(() => require('./service'));
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   const [email, setEmail] = useState('');
   const [user, setUser] = useState('');
   
-  // const setup = async () => {
-  //   await TrackPlayer.setupPlayer({});
-  //   await TrackPlayer.add(musiclibrary);
-  // };
-
-  // useEffect(() => {
-  //   setup();
-  // }, []);
-
-  // Trigger magic link for user to login / generate wallet
-  const login = async () => {
-    try {
-      console.log('HELLO LOGIN1', email, new Date())
-      await magic.auth.loginWithMagicLink({ email }) //.then(temp => console.log(temp));
-      console.log('we dont reach this part...')
-      magic.user.getMetadata().then(setUser);
-    } catch(err) {
-      alert(err);
-    }
+  const setup = async () => {
+    await TrackPlayer.setupPlayer({});
+    await TrackPlayer.add(musiclibrary);
   };
 
-  // Logout of Magic session
-  const logout = async () => {
-    await magic.user.logout();
-    setUser('');
-  };
+  useEffect(() => {
+    setup();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
         <Text style={styles.header}>test player</Text>
-        {/* <TrackListScreen /> */}
-      <Text style={styles.title}>Test Magic.link</Text>
-      {
-      !user ? 
-        <View>
-          <Text style={styles.header}>Login or Signup</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setEmail(text)}
-            value={email}
-            placeholder='Enter your email'
-          />
-          <View>
-            <Pressable style={styles.button} onPress={() => login()}><Text style={styles.buttonText}>Login</Text></Pressable>
-          </View>
-        </View> :
-        <View style={styles.view}>
-          <Text style={styles.text}>{user.email}</Text>
-          <Pressable style={styles.button} onPress={() => logout()}><Text style={styles.buttonText}>Logout</Text></Pressable>
-        </View>
-      }
+        <TrackListScreen />
       {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
